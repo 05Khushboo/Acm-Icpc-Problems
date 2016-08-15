27db9f6ff408f657064aa9b98b098cfc0e24edc2
Khushboo <khushuboogupta@yahoo.com>
2016-08-16 00:52:55 +0530

    update

--- Amritapuri Site, First Round -2015/Bhallaladeva.java
+++ Amritapuri Site, First Round -2015/Bhallaladeva.java
@@ -1,9 +1,9 @@
 /* package whatever; // don't place package name! */
-
+ 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
+ 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
@@ -35,4 +35,4 @@ class Ideone
 			
 		}
 	}
-}
+} 
